ad309f1969c7af0d4cd2023a1e75e6bfb5c7d27e
Diogo Esteves <149185661+dasesteves@users.noreply.github.com>
2024-03-27 15:08:13 +0000

    .

---
+++ Analise.R
@@ -0,0 +1,4 @@
+# explicação dos dados na pasta "dataset" ou "https://www.cbioportal.org/study/summary?id=prad_tcga_pan_can_atlas_2018", sua origem e relevância
+# - tarefas de preparação e de pré-processamento dos dados
+# - sumarização dos dados (estatística descritiva, exploração com recurso a gráficos)
+# - análise estatística univariada; análise de expressão diferencial e de enriquecimento
