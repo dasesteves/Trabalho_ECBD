2820ba281aae55038cf418b29154a2f49a4793fc
Diogo Esteves <149185661+dasesteves@users.noreply.github.com>
2024-03-27 15:44:18 +0000

    .

--- Analise.R
+++ Analise.R
@@ -1,7 +1,12 @@
-# explicação dos dados na pasta "dataset" ou "https://www.cbioportal.org/study/summary?id=prad_tcga_pan_can_atlas_2018", sua origem e relevância
-# - tarefas de preparação e de pré-processamento dos dados
+# explicação dos dados na pasta "dataset" ou "https://www.cbioportal.org/study/summary?id=prad_tcga_pan_can_atlas_2018", sua origem e relevância (alguém pode fazer este?)
+# - tarefas de preparação e de pré-processamento dos dados (a executar)
 # - sumarização dos dados (estatística descritiva, exploração com recurso a gráficos)
 # - análise estatística univariada; análise de expressão diferencial e de enriquecimento
+
+
+
+
+
 
 # Carregar os dados
 dados <- read.delim("caminho/para/o/arquivo.tsv")
