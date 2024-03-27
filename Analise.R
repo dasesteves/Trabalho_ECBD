f32d02d74e294e4cea3bba83dba18c4162197c39
dasesteves <dasesteves@hotmail.com>
2024-03-27 15:47:00 +0000

    ..

--- Analise.R
+++ Analise.R
@@ -1,5 +1,5 @@
-# explicação dos dados na pasta "dataset" ou "https://www.cbioportal.org/study/summary?id=prad_tcga_pan_can_atlas_2018", sua origem e relevância
-# - tarefas de preparação e de pré-processamento dos dados
+# explicação dos dados na pasta "dataset" ou "https://www.cbioportal.org/study/summary?id=prad_tcga_pan_can_atlas_2018", sua origem e relevância (alguém pode fazer este?)
+# - tarefas de preparação e de pré-processamento dos dados (a executar)
 # - sumarização dos dados (estatística descritiva, exploração com recurso a gráficos)
 # - análise estatística univariada; análise de expressão diferencial e de enriquecimento
 
@@ -7,16 +7,20 @@
 # Carregar os dados
 dados <- read.delim("caminho/para/o/arquivo.tsv")
 
-# Limpeza dos dados
-dados_limpos <- na.omit(dados) # Remove linhas com valores NA
+# Imputação pela média para variáveis contínuas
+dados_limpos <- dados
+for(i in 1:ncol(dados_limpos)) {
+  if(is.numeric(dados_limpos[,i])) {
+    dados_limpos[is.na(dados_limpos[,i]), i] <- mean(dados_limpos[,i], na.rm = TRUE)
+  }
+}
 
 # Sumarização dos dados
 summary(dados_limpos) # Estatísticas descritivas
 hist(dados_limpos$Idade) # Histograma da idade dos pacientes
 
-# Análise estatística univariada
-t.test(Idade ~ Status, data = dados_limpos) # Teste t para comparar a idade entre dois grupos de status
+# Supondo que 'dados_limpos' é o seu dataframe e 'colunas_geneticas' são as colunas de interesse
+dados_geneticos <- dados_limpos[, colunas_geneticas]
 
-# Análise de expressão diferencial (exemplo genérico)
-# Supondo que 'expressao' é um dataframe com dados de expressão gênica
-resultados_de <- DESeq2::DESeqDataSetFromMatrix(countData = expressao, ...)
+# Visualização com heatmap (exemplo)
+pheatmap::pheatmap(dados_geneticos)
