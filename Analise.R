59c502cf6f6c927f25473ef29a83a1202792cedc
Diogo Esteves <149185661+dasesteves@users.noreply.github.com>
2024-03-27 15:40:46 +0000

    ...

--- Analise.R
+++ Analise.R
@@ -3,20 +3,15 @@
 # - sumarização dos dados (estatística descritiva, exploração com recurso a gráficos)
 # - análise estatística univariada; análise de expressão diferencial e de enriquecimento
 
-
 # Carregar os dados
 dados <- read.delim("caminho/para/o/arquivo.tsv")
 
-# Limpeza dos dados
-dados_limpos <- na.omit(dados) # Remove linhas com valores NA
-
 # Sumarização dos dados
-summary(dados_limpos) # Estatísticas descritivas
-hist(dados_limpos$Idade) # Histograma da idade dos pacientes
+summary(dados) # Estatísticas descritivas
+hist(dados$Idade) # Histograma da idade dos pacientes
 
-# Análise estatística univariada
-t.test(Idade ~ Status, data = dados_limpos) # Teste t para comparar a idade entre dois grupos de status
+# Supondo que 'dados_limpos' é o seu dataframe e 'colunas_geneticas' são as colunas de interesse
+dados_genetica <- dados_limpos[, colunas_geneticas]
 
-# Análise de expressão diferencial (exemplo genérico)
-# Supondo que 'expressao' é um dataframe com dados de expressão gênica
-resultados_de <- DESeq2::DESeqDataSetFromMatrix(countData = expressao, ...)
+# Visualização com heatmap (exemplo)
+pheatmap::pheatmap(dados_geneticos)
