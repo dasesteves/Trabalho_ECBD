cc1230c295ec8f6ef310a0c11da09184c6a3ac64
afonsosa2002 <155247304+afonsosa2002@users.noreply.github.com>
2024-03-28 13:16:40 +0000

    Analise.R

--- Analise.R
+++ Analise.R
@@ -1,4 +1,9 @@
 # explicação dos dados na pasta "dataset" ou "https://www.cbioportal.org/study/summary?id=prad_tcga_pan_can_atlas_2018", sua origem e relevância (alguém pode fazer este?)
+
+Os dados que vão ser analisados neste trabalho provêm do projeto Pan-Cancer Atlas (TCGA), este projeto foi uma colaboração em larga escala de diversos investigadores de todo o mundo apoiado pelo National Cancer Institute (NCI) e pelo National Human Genome Research Institute (NHGRI).
+Neste trabalho serão analisadas 494 amostras obtidas através do perfil molecular de mais de 11.000 tumores de 33 diferentes tipos de cancro, as obtenções destes dados foram realizadas através da utilização de diversas técnicas que permitiram examinar as alterações moleculares em diferentes níveis, quer no DNA, RNA, na proteína e na epigenética.
+A relevância desses dados consiste na compreensão da origem, de onde, e de como os tumores se desenvolvem em humanos, a análise dos perfis moleculares de diferentes tipos de cancro terá como objetivo identificar padrões comuns, diferenças e temas emergentes entre eles, estes dados serão fundamentais para o desenvolvimento de tratamentos mais eficazes e personalizados ,para além disso, os dados servem como uma importante referência para futuras pesquisas sobre o cancro, fornecendo uma base sólida para futuros estudos e análises.
+
 # - tarefas de preparação e de pré-processamento dos dados (a executar)
 # - sumarização dos dados (estatística descritiva, exploração com recurso a gráficos)
 # - análise estatística univariada; análise de expressão diferencial e de enriquecimento
